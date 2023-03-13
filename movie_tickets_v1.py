337be74fefa0a79a95fcaf9412b9eb48eb55312c
laua5 <laua5@middleton.school.nz>
2023-03-14 11:08:36 +1300

    update

--- movie_tickets_v1.py
+++ movie_tickets_v1.py
@@ -2,3 +2,20 @@
 Welcome screen and set up variables
 Created by Alex Lau
 """
+
+
+# Component 1 - welcome screen and set up variables
+def sell_ticket():
+    print("******** Fanfair Movies - ticketing system ********\n")
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    total_sales = 0
+    tickets_sold = 0
+
+
+# Main Routine
+sell_ticket()
+
